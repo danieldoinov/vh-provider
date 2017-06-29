738d04e074787dcb5050b1270e4ab7a034950c48
Daniel Doinov <daniel@nexput.com>
2017-06-29 11:06:53 +0300

    Version 1.0 . Should do well enough for now.

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# vh-provider
+vh-provider
+=============
 Provides a JS functionality to enable "visual height" ( 100vh ) on HTML elements in Apple devices ( Safari... )
+
+This is very small jQuery plugin that gives you a replacement for vertical height on Apple devices and Safari in general. 
+
+Intallation
+-------------
+Just download the .zip from here and include dist/vh-provider.js into you project 
+
+How to use 
+------------
+You initialize the plugin as any other jQuery functionality. 
+
+
+`
+               
+           $(document).ready(function (){
+
+            let options = {
+            
+                debug:false, //display debug information in console
+                force:false, // force resizing on all browsers
+                breakpoints:{
+                    xs: 400,//default, can be skipped
+                    sm: 576,//default, can be skipped
+                    md: 768,//default, can be skipped
+                    lg: 1024,//default, can be skipped
+                    xl: 1200,//default, can be skipped
+                    xxl: 1400,//default, can be skipped
+                    xxxl: 1600,//default, can be skipped
+                    xxxxl: 1900,//default, can be skipped
+                    custom:992 //custom resolution with custom key
+                }
+            }
+
+            $('body').vh(options)
+        })
+
+
+You can target whatever element of the DOM to initialize upon. Its not necessary to use `$('body').vh()`.
+Only the current element and its children will be targeted .
+
+After initialization the DOM is searched for data-vh attributes.
+
+The HTML should look like that : 
+
+`
+    <some-element data-vh="30" data-vh-xs="50" data-vh-custom="100"></some-element>
+`
+
+You can add / skip ass much breakpoints as you'd like. 
+
+initializing the plugin without any options will give you the following breakpoints to work with 
+ 
+`
+
+    xs: 400,
+    sm: 576,
+    md: 768,
+    lg: 1024,
+    xl: 1200,
+    xxl: 1400,
+    xxxl: 1600,
+    xxxxl: 1900
+                    
+`
+
+__The plugin works as if its mobile first, meaning the rule applies from the specified resolution and UP__
+
+__Using data-vh without a suffix means the rule is global and not dependant on resolution. The default rule is overridden by the resolution specific rules__
+
+__Important:__ Do not try to define inline styles on the HTML tags that contain the data-vh definition as the plugin removes the style="" attribute. 
+
+__ENJOY! HOPE IT HELPS__
---
+++ vh-provider.js
@@ -0,0 +1,140 @@
+(function ($) {
+    "use strict";
+    var items;
+    var settings;
+    var currentBreakpoint;
+    var windowHeight;
+    $.fn.vh = function (options) {
+
+        settings = $.extend(true, {
+            // defaults
+            debug: false,
+            force: false,
+            breakpoints: {
+                xs: 400,
+                sm: 576,
+                md: 768,
+                lg: 1024,
+                xl: 1200,
+                xxl: 1400,
+                xxxl: 1600,
+                xxxxl: 1900
+            }
+        }, options);
+        //sort breakpoint by value ASC
+        var keysSorted = Object.keys(settings.breakpoints).sort(function (a, b) {
+            return settings.breakpoints[a] - settings.breakpoints[b]
+        });
+        var sortedBreakpoints = {};
+        for (var ind in keysSorted) {
+            sortedBreakpoints[keysSorted[ind]] = settings.breakpoints[keysSorted[ind]];
+        }
+        settings.breakpoints = sortedBreakpoints;
+
+        currentBreakpoint = getCurrentBreakpoint();
+
+        settings.debug ? console.log('Init VH') : false;
+        settings.debug ? console.log('Settings', settings) : false;
+        //do work ?
+        var work = settings.force;
+
+        if (!work) {
+            //check browser
+            work = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream || (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1);
+            settings.debug ? console.log('browser detection returns : ', work ? 'yes' : 'no') : false;
+        } else {
+            settings.debug ? console.log('Force work on all browsers') : false;
+        }
+
+        if (work) {
+            var selectors = '[data-vh]';
+            for (var key in settings.breakpoints) {
+                selectors += ',[data-vh-' + key + ']';
+            }
+            settings.debug ? console.log('Generated selectors : ', selectors) : false;
+
+            items = this.filter(selectors);
+            this.each(function (ind, el) {
+                items = items.add($(el).find(selectors))
+            });
+
+            if (items.length > 0) {
+                //have items. continue.
+                settings.debug ? console.log('Items found : ', items) : false;
+                resize(true);
+                $(window).resize(function () {
+                    if (windowHeight != window.innerHeight) {
+                        windowHeight = window.innerHeight;
+                        resize(true);
+                    } else {
+                        resize();
+                    }
+                })
+            }
+        }
+        return this;
+    };
+
+    //resizes the elements to the specified percentage of the window height
+    function resize(force) {
+        if (force || currentBreakpoint != getCurrentBreakpoint()) {
+            currentBreakpoint = getCurrentBreakpoint();
+            var height = window.innerHeight;
+            var res = getBreakpointSpecificKeysAndSelectors();
+            settings.debug ? console.log(res) : false;
+            items.removeAttr('style').filter(res.selectors).each(function (ind, el) {
+                if (res.keys) {
+                    var fallback = true;
+                    for (var ind in res.keys) {
+                        if ($(el).data('vh-' + res.keys[ind])) {
+                            $(el).height(height * ($(el).data('vh-' + res.keys[ind]) / 100));
+                            fallback = false;
+                            break;
+                        }
+
+                    }
+                    settings.debug?console.log('Fallback to general ( data-vh ) ? ', fallback ? 'yes' : 'no'):false;
+                    if (fallback) {
+                        if ($(el).data('vh')) {
+                            $(el).height(height * ($(el).data('vh') / 100));
+
+                        }
+                    }
+                } else {
+                    if ($(el).data('vh')) {
+                        $(el).height(height * ($(el).data('vh') / 100));
+
+                    }
+                }
+            })
+        }
+    }
+
+    function getCurrentBreakpoint() {
+        var result = 'all';
+        var width = window.innerWidth;
+        for (var key in settings.breakpoints) {
+            if (settings.breakpoints[key] <= width) {
+                result = key;
+            }
+        }
+        return result;
+    }
+
+    function getBreakpointSpecificKeysAndSelectors() {
+        var result = '[data-vh]';
+        if (currentBreakpoint != 'all') {
+            var keys = Object.keys(settings.breakpoints);
+            //slice from breakpoint up
+            keys = keys.slice(0, keys.indexOf(currentBreakpoint) + 1).reverse();
+            for (var key in keys) {
+                result += ',[data-vh-' + keys[key] + ']';
+            }
+        }
+        return {
+            selectors: result,
+            keys: keys ? keys : false
+        };
+    }
+
+})(jQuery);
